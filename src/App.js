1b9dbf14a0ba21823913a86d4a54cc58f3185e15
M-Ali-Coder <m.ali.coder.93@gmail.com>
2020-07-02 13:17:04 +0200

    Adding AppFooter component and make it responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./scss/App.scss";
+import { Switch, Route } from "react-router-dom";
 import UpperNav from "./components/UpperNav";
 import MainHeader from "./components/MainHeader";
 import MainNavbar from "./components/MainNavbar";
-import { Switch, Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
+import AppFooter from "./components/AppFooter";
 
 function App() {
   return (
@@ -12,10 +13,11 @@ function App() {
       <UpperNav />
       <MainHeader />
       <MainNavbar />
-
+      {/* React router dom routing */}
       <Switch>
         <Route exact path="/" render={() => <HomePage />} />
       </Switch>
+      <AppFooter />
     </div>
   );
 }
